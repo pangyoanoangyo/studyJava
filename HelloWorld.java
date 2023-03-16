2086538b64667c42a56b5ac94e66c4c914118b44
Coding, fall asleep here <97490317+pangyoanoangyo@users.noreply.github.com>
2023-03-16 13:02:18 +0900

    main

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Hello world는 못참지!
+
+System.out.println("Hello World!");
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");        
+    }
+}
